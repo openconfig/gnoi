dac5cbebbce08b56ce19e5259b4397a140f3255c
Sam Ribeiro <ribeiro@google.com>
2020-03-10 16:07:15 +0100

    Include os and factory_reset in proto build

--- compile_protos.sh
+++ compile_protos.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
+declare -a pp=(types common diag bgp cert file interface layer2 mpls system otdr wavelength_router os factory_reset)
+
 # Go
-for p in types common diag bgp cert file interface layer2 mpls system otdr wavelength_router; do
+for p in "${pp[@]}"; do
   protoc -I=$proto_imports --go_out=plugins=grpc:. $p/$p.proto
 done
 
