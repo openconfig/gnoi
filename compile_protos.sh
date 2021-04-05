c8062e090a8031d42180fad7465d77632ff235ae
Rob Shakir <robjs@google.com>
2021-04-05 16:21:05 -0700

    Update protobuf generation, add go_package option.

--- compile_protos.sh
+++ compile_protos.sh
@@ -5,6 +5,6 @@ proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
 # Go
 for p in types common diag bgp cert file interface layer2 mpls system os otdr wavelength_router; do
-  protoc -I=$proto_imports --go_out=plugins=grpc:. $p/$p.proto
+  protoc -I=$proto_imports --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative $p/$p.proto
 done
 
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/openconfig/gnoi
 go 1.15
 
 require (
-	github.com/golang/protobuf v1.4.3
+	github.com/golang/protobuf v1.5.0
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
 	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.26.0
 )
