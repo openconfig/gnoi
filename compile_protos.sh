2366b6c2047f9fda68502bc3788001a2193e4837
Rob Shakir <robjs@google.com>
2021-04-05 12:57:16 -0700

    Update infrastructure rules.

--- compile_protos.sh
+++ compile_protos.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
 # Go
-for p in types common diag bgp cert file interface layer2 mpls system otdr wavelength_router; do
+for p in types common diag bgp cert file interface layer2 mpls system os otdr wavelength_router; do
   protoc -I=$proto_imports --go_out=plugins=grpc:. $p/$p.proto
 done
 
