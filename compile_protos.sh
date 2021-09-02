17e1f67bed32a166a2d66a65d3a43756d0d51ba0
greg-dennis <gdennis@google.com>
2021-09-02 10:56:55 -0400

    compile protos with factory_reset included

--- compile_protos.sh
+++ compile_protos.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
 # Go
-for p in types common diag bgp cert file healthz interface layer2 mpls system otdr wavelength_router; do
+for p in types common diag bgp cert factory_reset file healthz interface layer2 mpls system otdr wavelength_router; do
   protoc -I="${proto_imports}" --go-grpc_out=. --go-grpc_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative $p/$p.proto
 done
 
