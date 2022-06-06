1010ff9f03743afff26c4350d3fbcdbbb623510b
Raj998 <raj.bansal.nig@gmail.com>
2022-06-06 16:59:49 +0530

    Revert "In progress cleanup of gNOI protobufs and repository."
    This reverts commit ef5cbad52ee39135ff03d7bee506ffb427cfa45c.

--- compile_protos.sh
+++ compile_protos.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 
 # Go
-for p in types common diag bgp cert file interface layer2 mpls system; do
+protoc -I=$proto_imports --go_out=. types.proto
+for p in bgp cert file interface layer2 mpls system; do
   protoc -I=$proto_imports --go_out=plugins=grpc:. $p/$p.proto
 done
  
