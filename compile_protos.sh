3c5421984341e83d370961ae4dda7fc8113ad6f2
Brian Neville <bneville@arista.com>
2022-09-08 14:06:12 +0100

    add os package to compile_protos.sh (#89)

--- compile_protos.sh
+++ compile_protos.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src/github.com/googleapis/googleapis:${GOPATH}/src"
 
 # Go
-for p in types common diag bgp cert factory_reset file healthz interface layer2 mpls system otdr wavelength_router packet_link_qualification; do
+for p in types common diag bgp cert factory_reset file healthz interface layer2 mpls system os otdr wavelength_router packet_link_qualification; do
   protoc -I="${proto_imports}" --go-grpc_out=. --go-grpc_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative $p/$p.proto
 done
 
