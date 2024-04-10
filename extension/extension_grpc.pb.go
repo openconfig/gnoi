11501326f55dcc20812889772325a8add2305160
Brian Neville <29639579+brianneville@users.noreply.github.com>
2024-04-10 16:27:01 +0100

    extension: rename ShowExtensions to ListExtensions

--- extension/extension_grpc.pb.go
+++ extension/extension_grpc.pb.go
@@ -19,7 +19,7 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Extension_ShowExtensions_FullMethodName     = "/gnoi.extension.Extension/ShowExtensions"
+	Extension_ListExtensions_FullMethodName     = "/gnoi.extension.Extension/ListExtensions"
 	Extension_FinalizeExtensions_FullMethodName = "/gnoi.extension.Extension/FinalizeExtensions"
 	Extension_InstallExtension_FullMethodName   = "/gnoi.extension.Extension/InstallExtension"
 	Extension_UninstallExtension_FullMethodName = "/gnoi.extension.Extension/UninstallExtension"
@@ -30,7 +30,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ExtensionClient interface {
 	// Shows the current state of the extensions on the system.
-	ShowExtensions(ctx context.Context, in *ShowExtensionsRequest, opts ...grpc.CallOption) (*ShowExtensionsResponse, error)
+	ListExtensions(ctx context.Context, in *ListExtensionsRequest, opts ...grpc.CallOption) (*ListExtensionsResponse, error)
 	// FinalizeExtensions reloads any services on the system for
 	// which extensions have been installed but which have not yet been
 	// reloaded with those extensions.
@@ -107,9 +107,9 @@ func NewExtensionClient(cc grpc.ClientConnInterface) ExtensionClient {
 	return &extensionClient{cc}
 }
 
-func (c *extensionClient) ShowExtensions(ctx context.Context, in *ShowExtensionsRequest, opts ...grpc.CallOption) (*ShowExtensionsResponse, error) {
-	out := new(ShowExtensionsResponse)
-	err := c.cc.Invoke(ctx, Extension_ShowExtensions_FullMethodName, in, out, opts...)
+func (c *extensionClient) ListExtensions(ctx context.Context, in *ListExtensionsRequest, opts ...grpc.CallOption) (*ListExtensionsResponse, error) {
+	out := new(ListExtensionsResponse)
+	err := c.cc.Invoke(ctx, Extension_ListExtensions_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -170,7 +170,7 @@ func (c *extensionClient) UninstallExtension(ctx context.Context, in *UninstallE
 // for forward compatibility
 type ExtensionServer interface {
 	// Shows the current state of the extensions on the system.
-	ShowExtensions(context.Context, *ShowExtensionsRequest) (*ShowExtensionsResponse, error)
+	ListExtensions(context.Context, *ListExtensionsRequest) (*ListExtensionsResponse, error)
 	// FinalizeExtensions reloads any services on the system for
 	// which extensions have been installed but which have not yet been
 	// reloaded with those extensions.
@@ -244,8 +244,8 @@ type ExtensionServer interface {
 type UnimplementedExtensionServer struct {
 }
 
-func (UnimplementedExtensionServer) ShowExtensions(context.Context, *ShowExtensionsRequest) (*ShowExtensionsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ShowExtensions not implemented")
+func (UnimplementedExtensionServer) ListExtensions(context.Context, *ListExtensionsRequest) (*ListExtensionsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListExtensions not implemented")
 }
 func (UnimplementedExtensionServer) FinalizeExtensions(context.Context, *FinalizeExtensionsRequest) (*FinalizeExtensionsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FinalizeExtensions not implemented")
@@ -269,20 +269,20 @@ func RegisterExtensionServer(s grpc.ServiceRegistrar, srv ExtensionServer) {
 	s.RegisterService(&Extension_ServiceDesc, srv)
 }
 
-func _Extension_ShowExtensions_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ShowExtensionsRequest)
+func _Extension_ListExtensions_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListExtensionsRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ExtensionServer).ShowExtensions(ctx, in)
+		return srv.(ExtensionServer).ListExtensions(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Extension_ShowExtensions_FullMethodName,
+		FullMethod: Extension_ListExtensions_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ExtensionServer).ShowExtensions(ctx, req.(*ShowExtensionsRequest))
+		return srv.(ExtensionServer).ListExtensions(ctx, req.(*ListExtensionsRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -357,8 +357,8 @@ var Extension_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*ExtensionServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "ShowExtensions",
-			Handler:    _Extension_ShowExtensions_Handler,
+			MethodName: "ListExtensions",
+			Handler:    _Extension_ListExtensions_Handler,
 		},
 		{
 			MethodName: "FinalizeExtensions",
